c6c97677f5e3f4775e71ff502e9b1cb590de01ab
Mustafizur <sayhemrahman@gmail.com>
2021-12-19 17:45:10 +0600

    added form for adding information name phone email and submit button

--- src/components/AddInformation/AddInformation.js
+++ src/components/AddInformation/AddInformation.js
@@ -2,8 +2,48 @@ import React from 'react';
 
 const AddInformation = () => {
     return (
-        <div>
-
+        <div className="container-fluid">
+            <h1 className="text-center text-danger py-3 display-2">Add Information</h1>
+            <div className="row">
+                <div className="col-md-6 p-5 mx-auto shadow">
+                    <form onSubmit={handleSubmit}>
+                        <div className="form-group">
+                            <input
+                                className="form-control"
+                                type="text"
+                                placeholder="Full name"
+                                value={name}
+                                onChange={(e) => setName(e.target.value)}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <input
+                                className="form-control"
+                                type="email"
+                                placeholder="Email"
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <input
+                                className="form-control"
+                                type="number"
+                                placeholder="Phone"
+                                value={phone}
+                                onChange={(e) => setPhone(e.target.value)}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <input
+                                className="btn btn-block btn-dark"
+                                type="submit"
+                                value="Add Information"
+                            />
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
