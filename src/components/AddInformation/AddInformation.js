4eef67f0444d016911999db213f1a090bd685b9a
Mustafizur <sayhemrahman@gmail.com>
2021-12-19 17:48:56 +0600

    added addinformation page  to collect information from redux

--- src/components/AddInformation/AddInformation.js
+++ src/components/AddInformation/AddInformation.js
@@ -1,6 +1,43 @@
 import React from 'react';
 
 const AddInformation = () => {
+
+
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const checkContactEmailExists = informations.filter((information) =>
+            information.email === email ? information : null
+        );
+        const checkContactPhoneExists = informations.filter((information) =>
+            information.phone === phone ? information : null
+        );
+
+        if (!email || !name || !phone) {
+            return toast.warning("EMpty field fill please");
+        }
+        if (checkContactEmailExists.length > 0) {
+            return toast.error("This email already exists!!");
+        }
+        if (checkContactPhoneExists.length > 0) {
+            return toast.error("This phone number already exists!!");
+        }
+
+        const data = {
+            id: informations.length > 0 ? informations[informations.length - 1].id + 1 : 0,
+            email,
+            name,
+            phone,
+        };
+
+        addInformation(data);
+        toast.success("Added Information Successfully");
+        history.push("/");
+    };
+
+
+
     return (
         <div className="container-fluid">
             <h1 className="text-center text-danger py-3 display-2">Add Information</h1>
